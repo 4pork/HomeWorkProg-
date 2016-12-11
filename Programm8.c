70e185fc03c01580776e94121bbab04b53a9e94d
4pork <danilopster@mail.ru>
2016-12-11 09:46:51 +0300

    Add files via upload

---
+++ Programm8.c
@@ -0,0 +1,71 @@
+#define ABSOLUTE_ZERO 273.15
+#define KELVIN 'K'
+#define CELSIUS 'C'
+#define FAHRENHEIT 'F'
+
+#include <stdio.h>
+
+float toCel(char from, float value)
+{
+    if (from == KELVIN)
+        return value - ABSOLUTE_ZERO;
+    if (from == FAHRENHEIT)
+        return (value - 32) / 1.8;
+    return 0;
+}
+
+float toKel(char from, float value)
+{
+    if (from == CELSIUS)
+        return value + ABSOLUTE_ZERO;
+    if (from == FAHRENHEIT)
+        return toKel(CELSIUS, toCel(FAHRENHEIT, value));
+    return 0;
+}
+
+float toFar(char from, float value)
+{
+    if (from == CELSIUS)
+        return (value * 1.8) + 32;
+    if (from == KELVIN)
+        return toFar(CELSIUS, toCel(KELVIN, value));
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    char scale;
+    float value;
+    printf("Input Conversion Values\n");
+    printf("C - Celsius\n");
+    printf("F - Fahrenheit\n");
+    printf("K - Kelvin\n");
+    scale = getchar();
+    printf("Input Temp Value\n");
+    scanf("%f", &value);
+    switch (scale) {
+        case KELVIN:
+        {
+            printf("Celsius degrease: %f\n", toCel(KELVIN, value));
+            printf("Fahrenheit degrease: %f\n", toFar(KELVIN, value));
+            break;
+        }
+        case CELSIUS:
+        {
+            printf("Kelvin degrease: %f\n", toKel(CELSIUS, value));
+            printf("Fahrenheit degrease: %f\n", toFar(CELSIUS, value));
+            break;
+        }
+        case FAHRENHEIT:
+        {
+            printf("Celsius degrease : %f\n", toCel(FAHRENHEIT, value));
+            printf("Kelvin degrease: %f\n", toKel(FAHRENHEIT, value));
+            break;
+        }
+        default:
+        {
+            printf("Wrong values, try again\n");
+            break;
+        }
+    }
+    return 0;
+}
